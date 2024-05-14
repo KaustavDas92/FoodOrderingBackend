e93fe9a916e09502d32bf0d005a03508db611138
KaustavDas92 <kaustavdas192@gmail.com>
2024-05-14 13:10:12 +0530

    fiirst push

---
+++ src/main/java/com/kaustav/fooddeliveringappbackend/FoodDeliveringAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kaustav.fooddeliveringappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveringAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveringAppBackendApplication.class, args);
+	}
+
+}
